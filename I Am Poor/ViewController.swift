d2f0d70697c6d699d53833493ee6b740fe52bbaa
Luis Locon <llocon@cognits.co>
2020-05-16 16:50:12 -0600

    Initial Commit

---
+++ I Am Poor/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  I Am Poor
+//
+//  Created by Luis Locón on 5/16/20.
+//  Copyright © 2020 Luis Locón. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
